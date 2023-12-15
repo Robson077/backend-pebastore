09deb80bf37cb48d81f917e6a9a682ef9c4d89a1
robson077 <minhamae830@gmail.com>
2023-12-14 21:52:44 -0300

    feat: preço

--- database/db.js
+++ database/db.js
@@ -6,20 +6,25 @@ const db = [
     type: "casa",
     nomeDaPessoa: "guga",
     aluga: true,
+    preço: 350,
     urlImg: "https://img.freepik.com/fotos-gratis/villa-com-piscina-de-luxo-espetacular-design-contemporaneo-arte-digital-imoveis-casa-casa-e-propriedade-ge_1258-150749.jpg?size=626&ext=jpg&ga=GA1.1.1805933026.1700498549&semt=sph"
     },
+
     {
         id: v4(),
         type: "casa",
         nomeDaPessoa: "jadson",
         aluga: false,
+        preço: 70000,
         urlImg: "https://img.freepik.com/fotos-gratis/casa-isolada-no-campo_1303-23773.jpg?size=626&ext=jpg&ga=GA1.1.1805933026.1700498549&semt=sph"
     },
+
     {
         id: v4(),
         type: "casa",
         nomeDaPessoa: "Robson",
         aluga: true,
+        preço: 550,
         urlImg: "https://img.freepik.com/fotos-gratis/renderizacao-3d-de-casa-aconchegante-moderna-com-piscina-e-estacionamento-para-venda-ou-aluguel_1142-40511.jpg?size=626&ext=jpg&ga=GA1.1.1805933026.1700498549&semt=sph"
     },
 
